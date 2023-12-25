38fa630b795a2c18d868a7a571052cbc2295354b
Adhithya-01 <117580174+Adhithya-01@users.noreply.github.com>
2023-12-25 10:44:40 +0530

    Update chp2_1.c

--- C/chp2_1.c
+++ C/chp2_1.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
     int n,i,temp=0;
     printf("Enter the 5-digit number : ");
     scanf("%d",&n);
-    for(i=0;i<n-1;i++)
+    for(i=0;i<5;i++)
     {
         temp+=(n%10);
         n/=10;
     }
-    temp+=n;
     printf("Sum of the digit = %d",temp);
     return 0;
 }
