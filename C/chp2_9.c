14ccf1fead4f467c972d949dfb1176177ce6d452
Adhithya-01 <117580174+Adhithya-01@users.noreply.github.com>
2023-12-25 11:36:26 +0530

    Update chp2_9.c

--- C/chp2_9.c
+++ C/chp2_9.c
@@ -6,32 +6,32 @@ int main()
     scanf("%d",&n);
     while(n!=0)
     {
-        if(n-100>0 || n-100==0)
+        if(n-100>=0)
         {
             temp+=1;
             n=n-100;
         }
-        else if(n-50>0 || n-50==0)
+        else if(n-50>=0)
         {
             temp+=1;
             n=n-50;
         }
-        else if(n-10>0 || n-10==0)
+        else if(n-10>=0)
         {
             temp+=1;
             n=n-10;
         }
-        else if(n-5>0 || n-5==0)
+        else if(n-5>=0)
         {
             temp+=1;
             n=n-5;
         }
-        else if(n-2>0 || n-2==0)
+        else if(n-2>=0)
         {
             temp+=1;
             n=n-2;
         }
-        else if(n-1>0 || n-1==0)
+        else if(n-1>=0)
         {
             temp+=1;
             n=n-1;
